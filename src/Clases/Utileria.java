ee3c9af8159b8970ff9bf852b27b5a3bb73d8f9b
Francisco-Back <fbacks@miumg.edu.gt>
2020-10-03 17:33:38 -0600

    creando la clase utileria y el metodo de retorno de nombre

---
+++ src/Clases/Utileria.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author Francisco Back
+ */
+public class Utileria {
+    
+ public String getNombreClase(Cliente cliente){
+     return Cliente.class.getSimpleName();
+ 
+    
+ } 
+}
