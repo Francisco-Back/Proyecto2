77371a1f83500f6a9e5f0eea09401bb490be73b8
Francisco-Back <fbacks@miumg.edu.gt>
2020-10-03 15:43:26 -0600

    se crea una clase para ventana

---
+++ src/Global/VentanaNew.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Global;
+
+import Controlador.Main;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Francisco Back
+ */
+public class VentanaNew {
+    
+    public static void CreadorWindows(String NombreV, String Titulo){
+              try{
+                  //
+             FXMLLoader loader=new FXMLLoader();
+        loader.setLocation(Main.class.getResource("/Ventanas/"+NombreV+".fxml")); 
+            // se inicia la nueva ventana
+        Pane ventana=(Pane) loader.load();
+        Stage stage=new Stage();
+          stage.setTitle(Titulo);
+         Scene scene = new Scene(ventana);
+         stage.setScene(scene);
+         
+         stage.show();
+            }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    
+    
+}
