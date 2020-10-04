923954290fa3481254055d577cd2c487378f0600
Francisco-Back <fbacks@miumg.edu.gt>
2020-10-03 19:40:37 -0600

    Creacion de clase producto

---
+++ src/Clases/Producto.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author Francisco Back
+ */
+public class Producto {
+     private int IdProducto;
+    private String NombreParte;
+    private String Codigo;
+    private String Marca;
+    private String Funcion;
+    
+    
+      int aux=2000;
+
+    public static int sigIdProducto(int IdProducto,int aux) {
+         return aux + IdProducto;
+    }
+
+    public Producto(int IdProducto) {
+       Cliente.sigIdCliente(IdProducto,aux);
+       aux=IdProducto;
+        this.IdProducto = IdProducto;
+    }
+
+    public Producto(int IdProducto, String NombreParte, String Codigo, String Marca, String Funcion) {
+        this(IdProducto);
+        this.IdProducto = IdProducto;
+        this.NombreParte = NombreParte;
+        this.Codigo = Codigo;
+        this.Marca = Marca;
+        this.Funcion = Funcion;
+       }
+
+    public int getIdProducto() {
+        return IdProducto;
+    }
+
+    public void setIdProducto(int IdProducto) {
+        this.IdProducto = IdProducto;
+    }
+
+    public String getNombreParte() {
+        return NombreParte;
+    }
+
+    public void setNombreParte(String NombreParte) {
+        this.NombreParte = NombreParte;
+    }
+
+    public String getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(String Codigo) {
+        this.Codigo = Codigo;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String Marca) {
+        this.Marca = Marca;
+    }
+
+    public String getFuncion() {
+        return Funcion;
+    }
+
+    public void setFuncion(String Funcion) {
+        this.Funcion = Funcion;
+    }
+
+    @Override
+    public String toString() {
+        Utileria utileria=new Utileria();
+        return "["+utileria.getNombreClase2(this)+"]" + "IdProducto=" + IdProducto + ", NombreParte=" + NombreParte + ", Codigo=" + Codigo + ", Marca=" + Marca + ", Funcion=" + Funcion + ", aux=" + aux + '}';
+    }
+    
+    
+    
+}
