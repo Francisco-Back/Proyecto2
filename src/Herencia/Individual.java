7c1d9089fa36b909c2b8a77d0b629536c7ebaf7c
Francisco-Back <fbacks@miumg.edu.gt>
2020-10-03 18:01:44 -0600

    se crearon los metodos y los atributos

--- src/Herencia/Individual.java
+++ src/Herencia/Individual.java
@@ -5,10 +5,47 @@
  */
 package Herencia;
 
+import Clases.Cliente;
+
 /**
  *
  * @author Francisco Back
  */
-public class Individual {
+public class Individual extends Cliente {
+
+    private String Dpi;
+    private int Descuento;
+
+    public Individual(String Dpi, int Descuento, int IdCliente, String NombreCliente, String Codigo, String Empresa, String Funcion) {
+        super(IdCliente, NombreCliente, Codigo, Empresa, Funcion);
+        this.Dpi = Dpi;
+        this.Descuento = Descuento;
+    }
+
+    public String getDpi() {
+        return Dpi;
+    }
+
+    public void setDpi(String Dpi) {
+        this.Dpi = Dpi;
+    }
+
+    public int getDescuento() {
+        return Descuento;
+    }
+
+    public void setDescuento(int Descuento) {
+        this.Descuento = Descuento;
+    }
+
+    @Override
+    public String toString() {
+        return  "Individual{" + "Dpi=" + Dpi + ", Descuento=" + Descuento + '}'+super.toString(); 
+    }
+
     
+
+   
+   
+
 }
