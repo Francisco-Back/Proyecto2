21aeda667b6854b08b4abe6b76ba7df4ac6e470a
Francisco-Back <fbacks@miumg.edu.gt>
2020-10-03 18:01:50 -0600

    se crearon los metodos y los atributos

--- src/Herencia/Empresa.java
+++ src/Herencia/Empresa.java
@@ -5,10 +5,58 @@
  */
 package Herencia;
 
+import Clases.Cliente;
+
 /**
  *
  * @author Francisco Back
  */
-public class Empresa {
+public class Empresa extends Cliente{
+    private String Contacto;
+    private int Descuento;
+
+    public Empresa(String Contacto, int Descuento, int IdCliente, String NombreCliente, String Codigo, String Empresa, String Funcion) {
+        super(IdCliente, NombreCliente, Codigo, Empresa, Funcion);
+        this.Contacto = Contacto;
+        this.Descuento = Descuento;
+    }
+
+    public String getContacto() {
+        return Contacto;
+    }
+
+    public void setContacto(String Contacto) {
+        this.Contacto = Contacto;
+    }
+
+    public int getDescuento() {
+        return Descuento;
+    }
+
+    public void setDescuento(int Descuento) {
+        this.Descuento = Descuento;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Empresa{" + "Contacto=" + Contacto + ", Descuento=" + Descuento + '}'+super.toString();
+    }
+
+   
+
+    
+
+  
+
+   
+
+  
+
+
+    
+   
+    
     
 }
