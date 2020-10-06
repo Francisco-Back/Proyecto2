8069fb344d0a7861abe053a46692c1d988f34283
Francisco-Back <fbacks@miumg.edu.gt>
2020-10-05 23:53:28 -0600

    se creo el contador

--- src/Clases/Producto.java
+++ src/Clases/Producto.java
@@ -10,35 +10,38 @@ package Clases;
  * @author Francisco Back
  */
 public class Producto {
-     private int IdProducto;
+
+    public static int correlativoCodigo = 2000;
+    private int IdProducto;
     private String NombreParte;
     private String Codigo;
     private String Marca;
     private String Funcion;
     private int precio;
-    
-    
-      int aux=2000;
 
-    public static int sigIdProducto(int IdProducto,int aux) {
-         return aux + IdProducto;
-    }
+    int aux = 2000;
 
     public Producto(int IdProducto) {
-       Producto.sigIdProducto(IdProducto,aux);
-       aux=IdProducto;
-        this.IdProducto = IdProducto;
+        this.IdProducto = 0;
     }
 
-    public Producto(int IdProducto, String NombreParte, String Codigo, String Marca, String Funcion,int precio) {
-        this(IdProducto);
-        this.IdProducto = IdProducto;
+    public Producto() {
+        this.IdProducto = 0;
+        this.NombreParte = "";
+        this.Codigo = "";
+        this.Marca = "";
+        this.Funcion = "";
+        this.precio = 0;
+    }
+
+    public Producto(int IdProducto, String NombreParte, String Codigo, String Marca, String Funcion, int precio) {
+        this.IdProducto = correlativoCodigo++;
         this.NombreParte = NombreParte;
         this.Codigo = Codigo;
         this.Marca = Marca;
         this.Funcion = Funcion;
-        this.precio=precio;
-       }
+        this.precio = precio;
+    }
 
     public int getIdProducto() {
         return IdProducto;
@@ -47,7 +50,6 @@ public class Producto {
     public void setIdProducto(int IdProducto) {
         this.IdProducto = IdProducto;
     }
-
 
     public String getNombreParte() {
         return NombreParte;
@@ -88,14 +90,11 @@ public class Producto {
     public void setPrecio(int precio) {
         this.precio = precio;
     }
-    
 
     @Override
     public String toString() {
-        Utileria utileria=new Utileria();
-        return "["+utileria.getNombreClase(this.getClass().getSimpleName())+"]" + "IdProducto=" + IdProducto + ", NombreParte=" + NombreParte + ", Codigo=" + Codigo + ", Marca=" + Marca + ", Funcion=" + Funcion + ", aux=" + aux + ",Precio"+precio;
+        Utileria utileria = new Utileria();
+        return "[" + utileria.getNombreClase(this.getClass().getSimpleName()) + "]" + "IdProducto=" + IdProducto + ", NombreParte=" + NombreParte + ", Codigo=" + Codigo + ", Marca=" + Marca + ", Funcion=" + Funcion + ", aux=" + aux + ",Precio" + precio;
     }
-    
-    
-    
+
 }
