4b2f129e11da7fd6718ac70dbd514beed0096558
Francisco-Back <fbacks@miumg.edu.gt>
2020-10-08 02:24:33 -0600

    se actualiza informacion final

--- src/Global/Global.java
+++ src/Global/Global.java
@@ -14,19 +14,22 @@ import Data.DataSistema;
  *
  * @author uservit37
  */
+// se crea un clase Global para almacenar crea objetos
 public class Global {
     
-    
+    // metodo que agrega un elemento  nuevo a arrayList
        public static void Ingreso(Cliente e){
              
           DataSistema.Array1().add(e);
           
     }
+        // metodo que agrega un elemento  nuevo a arrayList
         public static void Ingreso2(Cliente e){
              
           DataSistema.Array2().add(e);
           
     }
+         // metodo que agrega un elemento  nuevo a arrayList
         public static void IngresoPro(Producto e){
              
           DataSistema.Arrayp1().add(e);
