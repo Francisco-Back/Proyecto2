e972e012915cafef8f099990285942a1c5492200
Francisco-Back <fbacks@miumg.edu.gt>
2020-10-08 02:19:31 -0600

    se actualiza informacion final

--- src/Controlador/WindowsLoginController.java
+++ src/Controlador/WindowsLoginController.java
@@ -28,12 +28,13 @@ import javax.swing.JOptionPane;
  *
  * @author Francisco Back
  */
+// se crea el controlador de tipo WindowsLoginController
 public class WindowsLoginController implements Initializable {
 
 
     public PasswordField txtpassword;
     public TextField txtUsuario;
-
+// se crea le metod de validacion
     public void ValidarIngreso(ActionEvent actionEvent)  {
         // validamos ingreso usuario
        if(txtUsuario.getText().equals("francisco") && txtpassword.getText().equals("123")) {
@@ -52,7 +53,7 @@ public class WindowsLoginController implements Initializable {
             }
 
     
-
+// se crea el metodo para salir de la vetana
     public void salir(ActionEvent actionEvent) {
         JOptionPane.showMessageDialog(null, "Gracias por Utilizar Nuestra App");
         System.exit(0);
